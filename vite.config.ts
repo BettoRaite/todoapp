cece70b695e9a995693f8fc7538a4cf5f0ef5271
bettoraite <bettoraite@gmail.com>
2024-07-23 15:36:46 +0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: "index.html", // Ensure this points to your actual index.html
+		},
+	},
+	server: {
+		open: true, // Automatically open the app in the browser on server start
+	},
+});
